6f04c449ba73b2a739fc2b4b2578cf8f7ec25d2f
roshan <roshanpardeshi926@gmail.com>
2026-09-25 19:34:31 +0530

    add my task

--- chartgpt_question.py
+++ chartgpt_question.py
@@ -540,64 +540,314 @@
 ##print(li)
 
 
-hotel_data = {
-    "what is the hotel name": "Welcome to Royal Palace Hotel.",
-    
-    "where is the hotel located": "Royal Palace Hotel is located in Pune, Maharashtra.",
-    
-    "what rooms are available": "We have Single, Double, Deluxe and Suite rooms.",
-    
-    "what is the single room price": "The Single Room costs Rs. 1500 per night.",
-    
-    "what is the double room price": "The Double Room costs Rs. 2500 per night.",
-    
-    "what is the deluxe room price": "The Deluxe Room costs Rs. 3500 per night.",
-    
-    "what is the suite room price": "The Suite Room costs Rs. 5000 per night.",
-    
-    "what food do you serve": "We serve Veg and Non-Veg food.",
-    
-    "what veg food do you have": "We have Paneer Tikka, Veg Biryani, Masala Dosa and Veg Thali.",
-    
-    "what non veg food do you have": "We have Chicken Biryani, Chicken Tikka, Butter Chicken and Mutton Biryani.",
-    
-    "what are the hotel facilities": "We provide Wi-Fi, parking, room service, restaurant and laundry service.",
-    
-    "is wifi available": "Yes, free Wi-Fi is available for hotel guests.",
-    
-    "is parking available": "Yes, free parking is available for hotel guests.",
-    
-    "what are the check in timings": "Check-in time is 12:00 PM.",
-    
-    "what are the check out timings": "Check-out time is 11:00 AM.",
-    
-    "do you provide room service": "Yes, we provide 24-hour room service.",
-    
-    "how can i book a room": "You can book a room through the hotel reception or booking system.",
-    
-    "how can i cancel my booking": "You can cancel your booking by contacting the hotel reception.",
-    
-    "do you accept online payment": "Yes, we accept online payment, UPI, debit cards and credit cards.",
-    
-    "is breakfast included": "Breakfast is included with Deluxe and Suite room bookings.",
-    
-    "thank you": "You're welcome! Have a pleasant stay.",
-    
-    "hello": "Hello! Welcome to Royal Palace Hotel. How can I help you?"
-}
-
-
-
-
-while True:
-    question =input("Ask Question:-").lower()
-
-    if question in hotel_data.keys():
-        print(hotel_data[question])
-    else:
-        print("sorry sir i don't uderstand what you say")
+##hotel_data = {
+##    "what is the hotel name": "Welcome to Royal Palace Hotel.",
+##    
+##    "where is the hotel located": "Royal Palace Hotel is located in Pune, Maharashtra.",
+##    
+##    "what rooms are available": "We have Single, Double, Deluxe and Suite rooms.",
+##    
+##    "what is the single room price": "The Single Room costs Rs. 1500 per night.",
+##    
+##    "what is the double room price": "The Double Room costs Rs. 2500 per night.",
+##    
+##    "what is the deluxe room price": "The Deluxe Room costs Rs. 3500 per night.",
+##    
+##    "what is the suite room price": "The Suite Room costs Rs. 5000 per night.",
+##    
+##    "what food do you serve": "We serve Veg and Non-Veg food.",
+##    
+##    "what veg food do you have": "We have Paneer Tikka, Veg Biryani, Masala Dosa and Veg Thali.",
+##    
+##    "what non veg food do you have": "We have Chicken Biryani, Chicken Tikka, Butter Chicken and Mutton Biryani.",
+##    
+##    "what are the hotel facilities": "We provide Wi-Fi, parking, room service, restaurant and laundry service.",
+##    
+##    "is wifi available": "Yes, free Wi-Fi is available for hotel guests.",
+##    
+##    "is parking available": "Yes, free parking is available for hotel guests.",
+##    
+##    "what are the check in timings": "Check-in time is 12:00 PM.",
+##    
+##    "what are the check out timings": "Check-out time is 11:00 AM.",
+##    
+##    "do you provide room service": "Yes, we provide 24-hour room service.",
+##    
+##    "how can i book a room": "You can book a room through the hotel reception or booking system.",
+##    
+##    "how can i cancel my booking": "You can cancel your booking by contacting the hotel reception.",
+##    
+##    "do you accept online payment": "Yes, we accept online payment, UPI, debit cards and credit cards.",
+##    
+##    "is breakfast included": "Breakfast is included with Deluxe and Suite room bookings.",
+##    
+##    "thank you": "You're welcome! Have a pleasant stay.",
+##    
+##    "hello": "Hello! Welcome to Royal Palace Hotel. How can I help you?"
+##}
+##
+##
+##while True:
+##    question =input("Ask Question:-").lower()
+##
+##    if question in hotel_data.keys():
+##        print(hotel_data[question])
+##    else:
+##        print("sorry sir i don't uderstand what you say")
         
+
+
+
     
+##di = {
+##        "roshan pardeshi":"student",
+##        "sonu patil":"not student",
+##        "tushar patil":"college student"
+##    }
+
+##li = []
+
+##user = input("Enter the :-----")
+
+##for i in di.keys():
+##    li.append(i)
+
+##for i in range(0,len(li),1):
+##    if user in li[i]:
+##        print(di[li[i]])
+
+
+
+
+##li = [7, 1, 5, 3, 6, 4]
+##
+##
+##buy = li[1]
+##
+##sell = li[5]
+##
+##print(sell-buy)
+
+
+##for i in li:
+##    print(i)
+
+
+
+##li = [4, 1, 2, 4, 3, 4, 2, 1, 4]
+##
+##
+##
+##
+##li1 = []
+##
+##d1 ={}
+##for i in li:
+##    count = li.count(i)
+##    
+##    if count>1:
+##        if i not in li1:
+##            d1[i]=count
+##            li1.append(i)
+##
+##
+##print(d1)
+
+##li = [4, 1, 2, 4, 3, 4, 2, 1, 4]
+##
+##li1 = []
+##d1 = {}
+##
+##for i in li:
+##    count = li.count(i)
+##
+##    if count > 1:
+##        if i not in li1:
+##            d1[i] = count
+##            li1.append(i)
+##
+##most_frequent = None
+##frequency = 0
+##
+##for key, value in d1.items():
+##    if value > frequency:
+##        frequency = value
+##        most_frequent = key
+##
+##print("Most frequent element:", most_frequent)
+##print("Frequency:", frequency)
+##
+
+##li = [2, 7, 11, 15, 3, 6]
+##target = 9
+##
+##li1 = []
+##
+##for i in range(0,len(li),1):
+##    for j in range(i+1,len(li)):
+##        if li[i]+li[j]==target:
+##            li1.append((li[i],li[j]))
+##
+##
+##print(li1)
+            
+##li = [1, 2, 3, 5, 6, 7, 8, 9]
+##
+##
+##for i in range(1,len(li)+2,1):
+##    if i not in li:
+##        print(i)
+##    
+
+
+##li = [3, 1, 4, 1, 5, 9, 2, 6, 5]
+##
+##for i in li:
+##    count = li.count(i)
+##
+##    if count>1:
+##        print(i)
+##        break
+
+##li = [1, 2, 3, 4, 5, 6, 7]
+##
+##
+##
+##li1 = li[-1]
+##
+##li2 = li[-2]
+##
+##li3 = li[-3]
+##
+##li4 = li[-4]
+##
+##li.pop()
+##
+##li.pop()
+##
+##li.pop()
+##
+##li.pop()
+##
+##li.insert(0,li4)
+##
+##li.insert(1,li3)
+##
+##li.insert(2,li2)
+##
+##li.insert(3,li1)
+##
+##print(li)
+
+
+
+
+
+
+
+
+
+##
+##st = "({[]})"
+##
+##li = []
+##
+##for i in st:
+##    li.append(i)
+##
+##
+##for i in li:
+##
+##    if i.isdigit() or i.isalpha():
+##        pass
+##    else:
+##
+##        if len(li)==len(st):
+##            print("valid")
+
+##li = "aabbcdde"
+##
+##for i in li:
+##    count = li.count(i)
+##
+##    if count==1:
+##        print(i)
+##        break
+
+
+
+
+li = [1, 2, 3, 4]
+
+for i in range(0,len(li),1):
+    if li[i]:
+        print(li[i])
+
+    else:
+        print(li[i]*4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
